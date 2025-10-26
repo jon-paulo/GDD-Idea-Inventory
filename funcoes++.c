f5f6521074b7ab4c141ef28b4255397f14d3e80f
jon-paulo <joaopauloribeiro383@gmail.com>
2025-10-26 00:29:24 -0300

    Update funcoes++.c
    adicionei o "Editar ideia"
    e no editar_ideia também botei "recuperar ideia", que é só mudar o estado de "descartado" pra outra coisa

--- funcoes++.c
+++ funcoes++.c
@@ -119,11 +119,11 @@ void ler_ideia (struct Ideia* ideia)
 			scanf_flush(); //limpando queue por preocaução		
 
 			//caracteristicas
-			printf("Defina o personagem em %d palavras distintas:\n", C);
+			printf("Defina o personagem em %d palavras distintas(ou frases de até 64 caracteres):\n", C);
 			for (int i = 0; i < C; i++)
 			{
 				printf("%d° caracteristica: ", i+1);
-				scanf("%s", &ideia->carac[i][0]);
+				ler_frase(ideia->carac[i], P);
 			}
 			scanf_flush(); //limpando queue por preocaução
 
@@ -281,6 +281,7 @@ void criar_ideia(FILE* f){
 void buscar_ideia(FILE* f){
 	printf("Digite o ID da ideia que deseja buscar: ");
 	int id;
+	int edit = 0;
 	scanf("%d", &id);
 	scanf_flush();
 	
@@ -289,11 +290,226 @@ void buscar_ideia(FILE* f){
 	if (ler_ideia_arquivo(f, &ideia_encontrada, id)){
 		printf("||==============| Ideia Encontrada (ID %d) |==============||\n", id);
 		exibir_ideia(&ideia_encontrada);
+		printf("\n Deseja editar essa ideia? \n 1. Sim \n 2. Nao\n");
+		scanf("%d", &edit);
 	} else {
 		if (ideia_encontrada.id != id){
 			printf("A ideia de ID %d nao foi encontrada", id);
 		} else if (ideia_encontrada.est == E_descartado) {
 			printf("A ideia de ID %d foi descartada. \a\n", id);
+		}
+	}
+	if (edit == 1){
+		editar_ideia(f, id, &ideia_encontrada);
+	}
+}
+
+void menu_edit(FILE* f, int id, struct Ideia* ideia, int escolha){
+	if (escolha == 2){ // pro caso de soh querer recuperar uma ideia descartada.
+		char p;
+		enum Estado e = -1;
+		while (e == -1)
+		{
+			printf("Estado da ideia:\n(C)oncluído, (E)m Progresso, (A) Fazer, ou (D)escartado?\n");
+			scanf("%c", &p);
+			scanf_flush(); //limpando a queue para não duplicar o while
+			
+			switch (p)
+			{
+				case ('C'): 	e = E_concluido; 	break;
+				case ('E'): 	e = E_em_progresso;	break;
+				case ('A'): 	e = E_a_fazer;		break;
+				case ('D'):		e = E_descartado;	break;
+				default: 		e = -1;
+			}
+		}
+		ideia->est = e;
+		long posicao = (ideia->id - 1) * tamanho_registro;
+		if (fseek(f, posicao, SEEK_SET) != 0) return;
+	
+		fwrite(&ideia->id, sizeof(ideia->id), 1, f);
+		fwrite(&ideia->est, sizeof(ideia->est), 1, f);
+		return;
+	}
+	exibir_ideia(ideia);
+	while (escolha != 9){
+		printf("\n||===============| Editando Ideia |===============||\n");
+		printf("1. Editar Nome\n");
+		printf("2. Editar Estado\n");
+		printf("3. Editar Prioridade\n");
+		printf("4. Editar Resumo\n");
+		printf("5. Editar Categoria(vai apagar as subcategorias abaixo tambem)\n");
+		switch (ideia->Cat) {
+		case (C_Personagem):
+			printf("6. Editar idade:\n");
+			printf("7. Editar Categorias:\n", C);
+			break;
+		case (C_Item):
+			printf("6. Editar tipo do item:\n");
+			break;
+	
+		case (C_Cenario):
+			printf("6. Editar a cor predominante do cenário:\n");
+			break;
+		default: 
+			printf("erro no switch Cat em editar_ideia()");
+		}
+		printf("9. Sair\n");
+		printf("Escolha: ");
+		
+		if (scanf("%d", &escolha) != 1){
+			escolha = -1; //isso eh pra previnir um loop infinito caso scanf leia uma letra ou sla.
+		}
+		scanf_flush();
+		
+		switch(escolha)
+		{
+			case 1: 
+				printf("Nome da ideia (até %d caracteres):\n", P);
+				ler_frase(ideia->nome, P);
+				break;
+			case 2: 
+			{
+				char p;
+				enum Estado e = -1;
+				while (e == -1)
+				{
+					printf("Estado da ideia:\n(C)oncluído, (E)m Progresso, (A) Fazer, ou (D)escartado?\n");
+					scanf("%c", &p);
+					scanf_flush(); //limpando a queue para não duplicar o while
+				
+					switch (p)
+					{
+						case ('C'): 	e = E_concluido; 	break;
+						case ('E'): 	e = E_em_progresso;	break;
+						case ('A'): 	e = E_a_fazer;		break;
+						case ('D'):		e = E_descartado;	break;
+						default: 		e = -1;
+					}
+				}
+				ideia->est = e;
+				if ((ideia->est == E_concluido) || (ideia->est == E_descartado)) 
+				{			
+					ideia->prioridade = 0;  
+				}
+				break;
+			}
+			case 3: 
+				printf("Qual a prioridade da ideia no projeto?\n");
+				scanf("%d", &ideia->prioridade);
+				scanf_flush(); //limpando a queue por preocaução
+				break;
+			case 4:
+				printf("Resumo da ideia (até %d caracteres):\n", T);
+				ler_frase(ideia->resumo, T);
+				break;
+			case 5:
+			{
+				char p;
+	    		enum Categoria c = -1;
+				while (c == -1)
+				{
+					printf("(P)ersonagem, (I)tem, ou (C)enário?\n");
+					scanf("%c", &p);
+					scanf_flush(); //limpando a queue do stdin para não duplicar o while
+					
+					switch (p)
+					{
+						case ('P'): 	c = C_Personagem; 	break;
+						case ('I'): 	c = C_Item;			break;
+						case ('C'): 	c = C_Cenario;		break;
+						default: 	c = -1;
+					}
+				}
+				ideia->Cat = c;
+				
+				memset(&ideia->idade, 0, sizeof(UnionSize)); //apaga tudo
+				
+				switch (ideia->Cat) {
+					case (C_Personagem):
+						printf("Insira a idade do personagem:\n");
+						scanf("%d", &ideia->idade);
+						scanf_flush(); //limpando queue por preocaução
+						printf("Defina o personagem em %d palavras distintas(ou frases de até 64 caracteres):\n", C);
+						for (int i = 0; i < C; i++)
+						{
+							printf("%d° caracteristica: ", i+1);
+							ler_frase(ideia->carac[i], P);
+						}
+						scanf_flush();	
+						break;
+					case (C_Item):
+						printf("Insira o tipo do item:\n");
+						ler_frase(ideia->tipo, P);
+						break;
+					case (C_Cenario):
+						printf("Insira em hexadecimal a cor predominante do cenário:\n");
+						scanf("%x", &ideia->cor_predom);
+						scanf_flush(); //limpando queue por preocaução
+						break;
+					default: 
+					printf("erro no switch Cat em ler_ideia()");}
+			}
+			break;
+			case 6:
+				switch (ideia->Cat) {
+					case (C_Personagem):
+						printf("Insira a idade do personagem:\n");
+						scanf("%d", &ideia->idade);
+						scanf_flush(); //limpando queue por preocaução		
+						break;
+	
+					case (C_Item):
+						printf("Insira o tipo do item:\n");
+						ler_frase(ideia->tipo, P);
+						break;
+					case (C_Cenario):
+						printf("Insira em hexadecimal a cor predominante do cenário:\n");
+						scanf("%x", &ideia->cor_predom);
+						scanf_flush(); //limpando queue por preocaução
+						break;
+					default: 
+					printf("erro no switch Cat em ler_ideia()");}
+			break;
+			case 7:
+				printf("Defina o personagem em %d palavras distintas(ou frases de até 64 caracteres):\n", C);
+				for (int i = 0; i < C; i++)
+				{
+					printf("%d° caracteristica: ", i+1);
+					ler_frase(ideia->carac[i], P);
+				}
+				scanf_flush(); //limpando queue por preocaução
+			break;
+			
+			case 9: printf("Saindo... \n"); break;
+			default: printf("Entrada Invalida. \a \n"); //desde que descobri \a acho mt divertido
+		}
+	}
+	escrever_ideia(f, ideia);
+}
+
+void editar_ideia(FILE* f, int id, struct Ideia* ideia){
+	struct Ideia i;
+	if (!id) {
+		printf("Digite o ID da ideia que deseja editar: ");
+		scanf("%d", &id);
+		scanf_flush();
+		ideia = &i; //pega o endereço de i e usa como se o argumento tivesse sido dado antes.
+	}
+	if (ler_ideia_arquivo(f, ideia, id)){
+		printf("||==============| Ideia Encontrada (ID %d) |==============||\n", id);
+		exibir_ideia(ideia);
+		printf("\n O que deseja editar nessa ideia? \n ");
+		menu_edit(f, id, ideia, 0);
+	} else {
+		if (ideia->id != id){
+			printf("A ideia de ID %d nao foi encontrada", id);
+		} else if (ideia->est == E_descartado) {
+			printf("A ideia de ID %d foi descartada. Deseja recupera-la?\n 1. Sim \n 2. Nao \a\n", id);
+			int escolha = 0;
+			scanf("%d", &escolha);
+			scanf_flush();
+			if (escolha == 1) menu_edit(f, id, ideia, 2);
 		}
 	}
 }
@@ -342,8 +558,8 @@ int main () {
 		printf("\n||===============| GDD Idea Inventory |===============||\n");
 		printf("1. Criar nova ideia\n");
 		printf("2. Buscar ideia\n");
-		printf("3. Descartar ideia\n");
-		printf("4. Editar ideia\n");  // ainda to vendo isso
+		printf("3. Editar ideia\n");
+		printf("4. Descartar ideia\n");  // ainda to vendo isso
 		printf("5. Listar todas as ideias\n"); // e isso tambem
 		printf("6. EXCLUIR Ideia (Permanente)\n"); // mais um
 		printf("9. Sair\n");
@@ -358,7 +574,8 @@ int main () {
 		{
 			case 1: criar_ideia(f); break;
 			case 2: buscar_ideia(f); break;
-			case 3: descartar_ideia(f); break;	
+			case 3: editar_ideia(f, 0, NULL); break;
+			case 4: descartar_ideia(f); break;	
 			
 			case 9: printf("Saindo... \n"); break;
 	
